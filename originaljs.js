2d60b5ecf737030e61caa7c91f43efa91cf607c8
Ian <iankjera@gmail.com>
2021-04-10 16:36:59 -0400

    started adding JS, implemented buttons

--- originaljs.js
+++ originaljs.js
@@ -1,18 +1,5 @@
-const summary = document.createElement('p');
-const resultsContainer = document.querySelector('.results-container');
-const playerButtons = document.querySelectorAll("#playerSelects button");
-const winner = document.createElement('h3');
-
-playerButtons.forEach(button => button.addEventListener('click', e => {
-    let wpn = button.innerText.toLowerCase();
-    button.classList.add('selected');
-    summary.innerText = ''
-    playRound(wpn);
-}))
-
 //returns computer pick at random - rock paper or scissor 
 function computerPicks() {
-
     let ranNum = Math.floor((Math.random() * 3) + 1);
     let computerSelection;
     if (ranNum === 1) {
@@ -22,37 +9,45 @@ function computerPicks() {
     } else {
         computerSelection = 'Scissors';
     }
-    const computerButtons = document.querySelectorAll('#computerSelects button');
-    computerButtons.forEach(button => {
-        if (button.innerText === computerSelection) {
-            button.classList.add('selected')
-        }
-    })
     return computerSelection.toLowerCase();
 }
 
-//play 1 round of RPS, list selections
-function playRound(wpn) {
-    let computerSelection = computerPicks();
-    let playerSelection = wpn;
-    summary.innerText = `You selected ${playerSelection}, the computer selected ${computerSelection}`
-    resultsContainer.append(summary);
-    winner.innerText = 'You win'
-    if (computerSelection === playerSelection) {
-        winner.innerText = `Tie game, you both selected ${playerSelection}!`
-
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-
-    } else {
-        winner.innerText = 'You lose!'
+//for human -- function that returns rock paper or scissor
+function playerPicks() {
+    let input = prompt('Rock, Paper, or Scissors???').toLowerCase();
+    if (input === 'rock' || input === 'paper' || input === 'scissors') {
+        return input
     }
-    resultsContainer.append(winner);
 }
 
+//play 1 round of RPS, list selections
+function playRound() {
+    let computerSelection = computerPicks();
+    let playerSelection = playerPicks();
+    if (playerSelection === undefined) {
+        console.log('error, you must select either "Rock", "Paper", or "Scissors"')
+        return;
+    }
+    console.log(`You selected ${playerSelection}, the computer selected ${computerSelection}`);
+    //tie game, both selections match
+    if (computerSelection === playerSelection) {
+        console.log(`Tie game, you both selected ${playerSelection}!`)
+        return
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('you win!')
+        return true
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('you win!')
+        return true
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('you win!')
+        return true
+    } else {
+        console.log('You lose!')
+        return false
+    }
+}
+lorem
 //play a game of x rounds
 function game(rounds) {
     let playerScore = 0;
@@ -76,10 +71,3 @@ function game(rounds) {
         console.log(`Final results: You ${playerWins === true ? 'won' : 'lost'}! \n player: ${playerScore} \n computer: ${computerScore} `);
     }
 }
-
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return
-    this.classList.remove('selected')
-}
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
